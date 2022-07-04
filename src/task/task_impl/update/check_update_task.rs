1893c609d304e75a37d8aa2d648dc1755403a374
Orelvis Lago Vasallo <orelvis15@gmail.com>
2022-07-03 22:36:51 -0500

    fix update config

--- src/task/task_impl/update/check_update_task.rs
+++ src/task/task_impl/update/check_update_task.rs
@@ -7,13 +7,18 @@ use tar::Archive;
 use crate::env::Env;
 use strfmt::strfmt;
 use crate::{Success, Term};
-use crate::config::remote_config::{RemoteConfig, Update};
+use crate::config::remote_config::RemoteConfig;
 use crate::message::message::{Message, MessageData, MessageKind};
 use crate::task::task::Task;
 use crate::task::task_type::TaskType;
 use crate::utils::download_manager::download;
 use crate::utils::folders::Folder;
 use crate::utils::version_utils::get_last_cvm_version;
+
+const GIT_DOWNLOAD_URL: &str = "https://github.com/orelvis15/cvm/releases/download";
+const VERSION_PATTERN: &str = "v{version}";
+const NAME_PATTERN: &str = "cvm-{arch}.tar.gz";
+const FILE_NAME: &str = "cvm.tar.gz";
 
 pub struct CheckUpdateTask {
     pub input_data: CheckUpdateData,
@@ -42,7 +47,7 @@ impl Task for CheckUpdateTask {
             }));
         };
 
-        download_and_copy_version(&last_version, &config.init.git_assets, &config.update)
+        download_and_copy_version(&last_version)
     }
 
     fn check(self: &Self, _env: &mut Env, config: &RemoteConfig, term: &mut Term) -> Result<Success, Message> {
@@ -54,22 +59,24 @@ impl Task for CheckUpdateTask {
     }
 }
 
-fn download_and_copy_version(version: &String, base_url: &String, update_data: &Update) -> Result<Success, Message> {
+fn download_and_copy_version(version: &String) -> Result<Success, Message> {
     let home_dir = Folder::get_home_dir()?;
 
     let mut version_map = HashMap::new();
     version_map.insert("version".to_string(), version);
 
-    let ver = strfmt(&update_data.version_pattern, &version_map).unwrap();
+    let ver = strfmt(VERSION_PATTERN, &version_map).unwrap();
 
     let arch = std::env::consts::ARCH.clone();
     let mut arch_map = HashMap::new();
     arch_map.insert("arch".to_string(), arch.to_string());
 
-    let asset = strfmt(&update_data.name_pattern, &arch_map).unwrap();
-    let url = format!("{}/{}/{}", &base_url.as_str(), &ver.as_str(), &asset.as_str());
+    let asset = strfmt(&NAME_PATTERN, &arch_map).unwrap();
+    let url = format!("{}/{}/{}", GIT_DOWNLOAD_URL, &ver.as_str(), &asset.as_str());
 
-    let download_path = download(&url, format!("/{}", update_data.file_name).as_str())?;
+    println!("ahi {}", &url);
+
+    let download_path = download(&url, format!("/{}", &FILE_NAME).as_str())?;
 
     decompress(download_path, home_dir)
 }
